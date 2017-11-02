7fd5d72db4f798dfd306af7ad6795660b4302b44
Alex Turner <alexwilliamturner@gmail.com>
2017-11-02 11:30:32 +0000

    Added routes for logging in/out

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,9 @@ Rails.application.routes.draw do
   resources :club_members
   resources :event_members
   root      'home#home'
+  get       '/login',         to: 'users#sign_in'
+  get       '/signup',        to: 'users#sign_up'
+  get       '/logout',        to: 'devise/sessions#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
